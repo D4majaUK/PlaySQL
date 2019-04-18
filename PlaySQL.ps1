66052c538a2822b03a09595f4b5231eae2ae74fc
DamajaUK <D4majaUK@users.noreply.github.com>
2019-04-18 09:59:13 +0100

    Update PlaySQL.ps1
    More functionality to the script

--- PlaySQL.ps1
+++ PlaySQL.ps1
@@ -1,28 +1,33 @@
 $Global:SCCMSQLSERVER = "LOCALHOST" 
 $Global:DBNAME = "TEST" 
+
+$goFwd = 0
+
 Try 
 { 
-$SQLConnection = New-Object System.Data.SQLClient.SQLConnection 
-$SQLConnection.ConnectionString ="server=$SCCMSQLSERVER;database=$DBNAME;User ID=SA;Password=myStrongSnickers!&%B4r" 
-$SQLConnection.Open() 
-write-host "Connected"
+    $SQLConnection = New-Object System.Data.SQLClient.SQLConnection 
+    $SQLConnection.ConnectionString ="server=$SCCMSQLSERVER;database=$DBNAME;User ID=SA;Password=myStrongSnickers!&%B4r" 
+    $SQLConnection.Open() 
+    write-host "Connected"
+    $goFwd = 1
 } 
 catch 
 { 
     write-host "Failed to connect" 
 } 
 
+if ($goFwd -eq 1) {
+    $query = "SELECT * FROM Hello"
+    $command = $SQLConnection.CreateCommand()
+    $command.CommandText = $query
+    $result = $command.ExecuteReader()
 
-$query = "SELECT * FROM Hello"
-$command = $SQLConnection.CreateCommand()
-$command.CommandText = $query
-$result = $command.ExecuteReader()
+    $table = new-object "System.Data.DataTable"
+    $table.Load($result)
+    $table | select-object @{Name="ID";Expression={$_.id}}, @{Name="First";Expression={$_.first}}, @{Name="Last";Expression={$_.last}}
+    $result.Close()
 
-$table = new-object "System.Data.DataTable"
-$table.Load($result)
-$table | select-object @{Name="ID";Expression={$_.id}}, @{Name="First";Expression={$_.first}}, @{Name="Last";Expression={$_.last}}
-$result.Close()
-
-$SQLConnection.Close() 
+    $SQLConnection.Close() 
+}
 
 docker
