05457f0d4cba0a9a5e5fb0e3fbe01eb5cd5c5041
DamajaUK <D4majaUK@users.noreply.github.com>
2019-04-23 10:18:27 +0100

    Add files via upload
    Adding the GetSQL.ps1 script to find all the scripts

---
+++ GetSQL.ps1
@@ -0,0 +1,26 @@
+$release = "2019042019"
+  
+$files = gci "$release\*.sql" | sort-object -property Name
+$files | Format-Table
+
+$sqlcount = 0
+foreach($file in $files) {
+	$name = "2019042019\" + $file.Name
+	write-host -NoNewLine "Reading sql file...."
+	$name = "2019042019\" + $file.Name
+	write-host -fore yellow $name
+	$query = get-content $name
+	
+	if ($query.ToUpper() -match "UPDATE" -or $query.ToUpper() -match "SET") {
+		write-host -fore cyan "  " $query
+		write-host -NoNewLine "   "
+		write-host -back cyan -fore black "--- WARNING!!! I found a script that contains the following (UPDATE and SET) ---"
+	} elseif ($query.ToUpper() -notmatch "DELETE FROM" -and $query.ToUpper() -notmatch "TRUNCATE TABLE" -and $query.ToUpper() -notmatch "DROP TABLE" -and $done -eq 0) {
+		write-host -fore cyan "  " $query
+	} else {
+		write-host -fore cyan "  " $query
+		write-host -NoNewLine "   "
+		write-host -back red "--- WARNING!!! I found a script that contains one of the following (DELETE FROM, TRUNCATE TABLE, DROP TABLE) ---"
+	}
+	write-host ""
+}
